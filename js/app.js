e95c4d7aba21c81e6e1b016dcdac1ed75fbd6344
krishani29 <krishani29@users.noreply.github.com>
2020-04-28 17:10:41 +0530

    done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 console.log("welcome to notes app. this is app.js");
-showNotes();
+//showNotes();
 //if user adds a note, add into the local storage
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesobj = [];
@@ -11,10 +12,21 @@ addBtn.addEventListener("click", function (e) {
     else {
         notesobj = JSON.parse(notes);
     }
-    notesobj.push(addTxt.value);
+
+  let myobj = {
+        title: addTitle.value,
+        text: addTxt.value
+     }
+
+    notesobj.push(myobj);
     localStorage.setItem("notes", JSON.stringify(notesobj));
     addTxt.value = "";
+<<<<<<< HEAD
+    addTitle.value="";
     // console.log(notesobj);
+=======
+    console.log(notesobj);
+>>>>>>> 1faf9f072a6e927c071b4dd42083bc87267cdb27
     showNotes();
 })
 //function to show elements from localstorage
@@ -30,8 +42,8 @@ function showNotes() {
     notesobj.forEach(function (element, index) {
         html += `<div class="noteCard card my-2 mx-2" style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title">Note ${index + 1}</h5>
-          <p class="card-text"> ${element}</p>
+          <h5 class="card-title">${element.title}</h5>
+          <p class="card-text"> ${element.text}</p>
           <button id="${index}" class="btn btn-primary" onclick="deleteNote(this.id)">Delete Note</button>
         </div>
       </div>`;
@@ -39,12 +51,16 @@ function showNotes() {
 
     });
     let notesElm = document.getElementById('notes');
-    if (notes.length!= 0) 
+<<<<<<< HEAD
+    if (notes.length!=0) 
+=======
+    if (notes.length>0) 
+>>>>>>> 1faf9f072a6e927c071b4dd42083bc87267cdb27
     {
         notesElm.innerHTML = html;
     }
     else {
-        notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes. `;
+        notesElm.innerHTML = `<h1>Nothing to show! Use "Add a Note" section above to add notes.</h1> `;
 
     }
 }
@@ -73,7 +89,7 @@ search.addEventListener("input", function () {
     let noteCard = document.getElementsByClassName('noteCard');
     Array.from(noteCard).forEach(function (element) {
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        // console.log(cardTxt);
+        console.log(cardTxt);
         if (cardTxt.includes(inputVal)) {
             element.style.display = "block";
         }
